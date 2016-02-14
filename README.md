32e9e751ada41600b69bb235144eef827c7b510a
Centora <oksana.blonskaya@gmail.com>
2016-02-14 22:11:27 +0200

    Get photo list from remote server [0.0.1]

---
+++ README.md
@@ -0,0 +1,11 @@
+> Мой проект
+
+Как его установить?
+
+1. git clone https://github.com/centora/dz-port.git
+
+2. npm i
+
+3. bower i
+
+4. gulp
---
+++ app/js/main.js
@@ -0,0 +1,78 @@
+var appPhotos = angular.module("app-photos", [
+	'ngMaterial',
+	'ui.router',
+	'ngMessages',
+	'ui.filters'
+]);
+
+appPhotos.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/photo-list");
+	$stateProvider
+		.state('photo-list', {
+			url: "/",
+			templateUrl: "photo-list.html",
+			controller: "PhotoListController"
+		}).state('album', {
+			url: "/album/:id",
+			templateUrl: "album.html",
+		  controller: "AlbumController"
+		}).state('image', {
+			url: "/image/2",
+			templateUrl: "photo.html",
+			controller: "ImageController"
+		});
+});
+
+var root = 'http://jsonplaceholder.typicode.com';
+appPhotos.service("photoService", function($http, $q){
+	var deferred = $q.defer();
+	$http.get(root + '/photos').then(function(data){
+		deferred.resolve(data);
+	});
+	this.getPhotos = function(){
+		return deferred.promise;
+	}
+	this.getPhotos = function(){
+		return deferred.promise;
+	}
+});
+appPhotos.controller("PhotoListController", function($scope, photoService){
+	$scope.allImages = "";
+	var promise = photoService.getPhotos();
+	promise.then(function(data){
+		$scope.allImages = data.data;
+	});
+});
+
+appPhotos.service("getAlbumListService", function(){
+
+});
+
+appPhotos.controller("AlbumController", function($scope, photoService, $state, $location){
+	$scope.images = "";
+	$scope.url = $location.$$url;
+	$scope.urlAlbumId = parseInt($scope.url.replace("/album/",""));
+	console.log($scope.urlAlbumId);
+
+	var promise = photoService.getPhotos();
+	promise.then(function(data){
+		$scope.images = data.data;
+	});
+});
+
+appPhotos.service("getAlbumListService", function(){
+
+});
+/*
+
+appPhotos.controller("PhotoListController", function(){
+	console.log("photo list")
+});
+
+appPhotos.controller("AlbumController", function(){
+
+});
+
+appPhotos.controller("ImageController", function(){
+
+});*/
